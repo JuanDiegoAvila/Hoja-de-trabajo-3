a1c6fe4cfb90e4d8553501aa661da8ce3930f524
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-02-10 22:53:31 -0600

    tests gnome, merge, quick

--- Test/SortingTest.java
+++ Test/SortingTest.java
@@ -5,22 +5,36 @@ import static org.junit.Assert.*;
 
 public class SortingTest {
 
+    public Comparable<Integer>[] list;
     @Test
     public void gnomeSort() {
+        Integer[] data = {3,7,5,1,6};
+        Integer[] expected = {1,3,5,6,7};
 
+        Sorting.gnomeSort(data, data.length);
+
+        assertArrayEquals(expected, data);
 
     }
 
     @Test
     public void mergeSort() {
+        Integer[] data = {100,7,5,50,6};
+        Integer[] expected = {5,6,7,50,100};
 
+        Sorting.mergeSort(data,0, data.length-1);
 
-
+        assertArrayEquals(expected, data);
     }
 
     @Test
     public void quickSort() {
+        Integer[] data = {30,27,5,100,6};
+        Integer[] expected = {5,6,27,30,100};
 
+        Sorting.quickSort(data,0, data.length-1);
+
+        assertArrayEquals(expected, data);
 
     }
 
