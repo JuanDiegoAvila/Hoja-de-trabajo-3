ed89c4fca9b66c159365f25d3bfdd44a3773f3e6
Daniel Gonzalez <gon20293@uvg.edu.gt>
2021-02-13 17:09:24 -0600

    Graficas de complejidad

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Generador generador = new Generador();    
+        Integer[] data;
+        generador.generar(1000);
+        data = new Integer[1000];
+        try{
+            Scanner input = new Scanner(new File("numeros.txt"));
+            int cont = 0;
+            while(input.hasNextLine()){
+                data[cont] = Integer.parseInt(input.nextLine().trim());
+                cont++;
+            }
+            input.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        Sorting.gnomeSort(data, data.length);
+        Sorting.gnomeSort(data, data.length);
+        //Sorting.mergeSort(data, 0, data.length-1);
+        //Sorting.quickSort(data, 0, data.length-1);
+        //Sorting.radixSort(data);
+        //Sorting.bubbleSort(data);
+
+        for(int j = 0; j<data.length;j++){
+            System.out.println(data[j]+" ");
+        }
+        System.out.println();
+    }
+}
